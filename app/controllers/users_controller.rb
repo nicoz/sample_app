280d6c0fefbc0120152dd9af0e70bc5660b5c527
Nicolas Zuasti <nicozuasti@gmail.com>
2011-09-12 18:14:51 -0300

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,12 @@ class UsersController < ApplicationController
 	@user = User.new(params[:user])
 	if @user.save
 		# Handle a successful save.
+		sign_in @user
 		flash[:success] = "Welcome to the Sample App!"
 		redirect_to @user
 	else
 		@title = "Sign up"
+		@user.password = ""
 		render 'new'
 	end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SamleApp::Application.routes.draw do
-  resources :users
 
+  resources :users
+  resources :sessions, :only => [ :new, :create, :destroy ]
+  
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout',:to => 'sessions#destroy'
+  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
